f8983e7f805a22be90a2afff8e54fe46692a97d5
home-22 <armin.jasarevic.25@gmail.com>
2022-10-05 22:54:51 +0200

    cardd

--- lib/pages/Home_Page.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cardd/widgets/Product_Card.dart';
-import 'package:cardd/widgets/Product_Category.dart';
-
-import 'package:flutter/material.dart';
-
--- lib/widgets/Shopping_Card.dart
+++ lib/widgets/Shopping_Card.dart
@@ -1,6 +1,6 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class ShoppingCard extends StatelessWidget {
   const ShoppingCard({Key? key}) : super(key: key);
@@ -8,7 +8,43 @@ class ShoppingCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.lime[100],
+        centerTitle: true,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.blue,
+          ),
+        ),
+        title: Text(
+          'Kupovina',
+          style: TextStyle(
+              fontSize: 22, color: Colors.blue, fontWeight: FontWeight.w500),
+        ),
+      ),
+      body: Container(
+        color: Colors.lime[100],
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  child: Image.asset('images/3.png'),
+                ),
+              ],
+            ),
+            Column(
+              children: [Text('Neki text')],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
